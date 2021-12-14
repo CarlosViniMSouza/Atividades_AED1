ff85aa08f9c3947bcf8e2c0c9355e8a2edb78661
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-12-14 10:41:53 -0300

    Pilha.c -> add | Fila.c -> alt

--- Atividades/Atividade09.c
+++ Atividades/Atividade09.c
@@ -4,7 +4,7 @@ Nome     : Benevaldo Pereira Gonçalves
 */
 
 /*
-Assunto: Fila Encadeada
+Assunto: Pilha Encadeada
 */
 
 #include <stdio.h>
@@ -25,12 +25,12 @@ typedef struct NO {
 // [**] : Significa um ponteiro que aponta
 //        para outro ponteiro.
 
-void inicializar(TNO **inicio,TNO **fim);
-void inserir(TNO **inicio,TNO **fim);
-void remover(TNO **inicio,TNO **fim);
-void removerTodos(TNO **inicio,TNO **fim);
-void exibirFila(TNO *inicio);
-void calcularTamanhoByteFila(TNO *inicio);
+void inicializar(TNO **topo);
+void inserir(TNO **topo);
+void remover(TNO **topo);
+void removerTodos(TNO **topo);
+void exibirPilha(TNO *topo);
+void calcularTamanhoBytePilha(TNO *topo);
 void menu();
 int ObterOpcaoMenu();
 
@@ -45,36 +45,35 @@ int main()
 void menu(){
   int opcao;
   // Declarar os ponteiros para o
-  // início e fim da fila encadeada
-  TNO *inicio;
-  TNO *fim;
+  // topo da pilha
+  TNO *topo;
 
-  // Inicializar os ponteiros início e fim
-  inicializar(&inicio,&fim);
+  // Inicializar o ponteiro topo
+  inicializar(&topo);
 
   do{
    opcao = ObterOpcaoMenu();
    switch (opcao)
    {
      case 1:
-       inserir(&inicio,&fim);
+       inserir(&topo);
        break;
      case 2:
-       exibirFila(inicio);
+       exibirPilha(topo);
        break;
      case 3:
-       remover(&inicio,&fim);
+       remover(&topo);
        break;
      case 4:
-       removerTodos(&inicio,&fim);
+       removerTodos(&topo);
        break;
      case 5:
-       calcularTamanhoByteFila(inicio);
+       calcularTamanhoBytePilha(topo);
        break;
      case 6:
        // Para sair do programa deve-se
        // desalocar toda memória alocada.
-       removerTodos(&inicio,&fim);
+       removerTodos(&topo);
        printf("\n");
        printf("\n***************************");
        printf("\n** Programa Finalizado.  **");
@@ -97,15 +96,15 @@ int ObterOpcaoMenu(){
   int opcao;
   printf("\n");
   printf("************************************\n");
-  printf("*          Fila Encadeada          *\n");
+  printf("*          Pilha Encadeada         *\n");
   printf("************************************\n");
   printf("*             Menu                 *\n");
   printf("************************************\n");
   printf("* [1] - Inserir                    *\n");
-  printf("* [2] - Listar Fila                *\n");
+  printf("* [2] - Listar Pilha               *\n");
   printf("* [3] - Remover                    *\n");
   printf("* [4] - Remover todos os elementos *\n");
-  printf("* [5] - Tamanho em bytes da fila   *\n");
+  printf("* [5] - Tamanho em bytes da pilha  *\n");
   printf("* [6] - Sair                       *\n");
   printf("************************************\n");
   printf("Entre com a opcao = ");
@@ -114,16 +113,15 @@ int ObterOpcaoMenu(){
 }
 
 // Inicializa uma
-// Lista encadeada simples
-void inicializar(TNO **inicio,TNO **fim){
+// Pilha encadeada simples
+void inicializar(TNO **topo){
  // OBS: NULL é um valor que indica
  // um endereço de memória vazio.
- *inicio = NULL;
- *fim    = NULL;
+ *topo = NULL;
 }
 
-// Inseri um novo elemento no final da Fila
-void inserir(TNO **inicio,TNO **fim){
+// Inseri um novo elemento no topo da pilha
+void inserir(TNO **topo){
  int elemento;
  // Declarar novo NO
  TNO* novoNO;
@@ -133,81 +131,75 @@ void inserir(TNO **inicio,TNO **fim){
  // Alocar memória dinamicamente
  novoNO = (TNO*) malloc(sizeof(TNO));
  // Verificar SE é o primeiro
- // elemento da Fila Encadeada
- if( *inicio==NULL){
-    // Primeiro elemento da fila Endadeada
-    // O ponteiro do início e do fim
-    // apontam para o mesmo elemento.
-    novoNO->numero  = elemento; //6
+ // elemento da Pilha Encadeada
+ if( *topo==NULL){
+    // Primeiro elemento da Pilha Endadeada
+    novoNO->numero  = elemento;
     novoNO->proximo = NULL;
-    *inicio = novoNO;
-    *fim    = novoNO;
+    *topo = novoNO;
  }else{
-    // Já existe elemento na fila,
-    // inseri no fim
-    novoNO->numero  = elemento; // 7,
-    novoNO->proximo = NULL;
-    (*fim)->proximo = novoNO;
-    *fim  = novoNO;
+    // Já existe elemento na pilha,
+    // inseri no topo da pilha
+    novoNO->numero  = elemento;
+    novoNO->proximo = *topo;
+    *topo = novoNO;
  }
 }
 
-// Remove o primeiro elemento da Fila
-void remover(TNO **inicio,TNO **fim){
+// Remove o primeiro elemento do topo da pilha
+void remover(TNO **topo){
  TNO * removerNO    = NULL;
 
- if(*inicio==NULL){
-   // Fila está fazia.
+ if(*topo==NULL){
+   // Pilha está fazia.
    printf("\n");
    printf("\n*************************");
-   printf("\n** Fila está vazia...  **");
+   printf("\n** Pilha está vazia... **");
    printf("\n*************************");
    printf("\n");
  }else{
    // Verificar SE existe apenas
-   // um elemento na fila
-   if (*inicio == *fim){
-     // A fila só tem um elemento
-     removerNO = *inicio;
-     *inicio = NULL;
-     *fim    = NULL;
+   // um elemento na pilha
+   if ((*topo)->proximo == NULL){
+     // A pilha só tem um elemento
+     removerNO = *topo;
+     *topo = NULL;
      // Remove NO e libera memória
      free(removerNO);
    }else{
-     // A fila tem mais de um elemento
-     removerNO = *inicio;
-     *inicio   = (*inicio)->proximo;
+     // A pilha tem mais de um elemento
+     removerNO = *topo;
+     *topo   = (*topo)->proximo;
      // Remove NO e libera memória
      free(removerNO);
    }
  }
 }
 
-// Remove todos os elmentos da fila
-void removerTodos(TNO **inicio,TNO **fim){
+// Remove todos os elmentos da pilha
+void removerTodos(TNO **topo){
  TNO *remover;
- TNO *lista;
+ TNO *pilha;
 
- if (inicio == NULL){
-   // Fila esta vazia
+ if (topo == NULL){
+   // Pilha esta vazia
    printf("\n");
    printf("\n***********************");
-   printf("\n** Fila está vazia.  **");
+   printf("\n** Pilha está vazia.  **");
    printf("\n***********************");
    printf("\n");
  }else{
-   // Existe elemento na Fila
-   // Ponteiro para navegar na Fila
-   lista = *inicio;
-   while(lista != NULL){
-     remover = lista;
-     lista   = lista->proximo;
+   // Existe elemento na Pilha
+   // Ponteiro para navegar na Pilha
+   pilha = *topo;
+   while(pilha != NULL){
+     remover = pilha;
+     pilha   = pilha->proximo;
      // Remover e desalocar memória
      free(remover);
    }
-   // Reiniciar a fila
-   *inicio = NULL;
-   *fim    = NULL;
+   // Reiniciar a pilha
+   *topo = NULL;
    printf("\n");
    printf("*************************************\n");
    printf(" Todos os elementos foram removidos. \n");
@@ -215,50 +207,48 @@ void removerTodos(TNO **inicio,TNO **fim){
  }// if
 }
 
-// Exibe todos os elementos da fila
-void exibirFila(TNO *inicio){
- // Ponteiro para navegar na Fila
- if (inicio == NULL){
-   // Fila esta vazia
+// Exibe todos os elementos da pilha
+void exibirPilha(TNO *topo){
+ // Ponteiro para navegar na Pilha
+ if (topo == NULL){
+   // Pilha esta vazia
    printf("\n");
    printf("\n********************");
-   printf("\n** Fila   vazia... **");
+   printf("\n** Pilha  vazia... **");
    printf("\n********************");
    printf("\n");
  }else{
-   // Existe elemento na Fila
+   // Existe elemento na Pilha
    printf("\n");
    printf("*****************************************************\n");
-   printf("Lista todos os elementos a partir do início da Fila *\n");
+   printf("Lista todos os elementos a partir do início da Pilha*\n");
    printf("*****************************************************\n");
-   while(inicio != NULL){
-    printf("Elemento = %d\n",inicio->numero);
-    inicio = inicio->proximo;
+   while(topo != NULL){
+    printf("Elemento = %d\n",topo->numero);
+    topo = topo->proximo;
    }
  }// if
 }// exibirLista
 
 // Calcula a quntidade de bytes alocada
-// pela fila encadeada com os seus
-// respectivos marcadores de início e fim
-void calcularTamanhoByteFila(TNO *inicio){
+// pela pilha encadeada com o seu
+// respectivo marcador do topo
+void calcularTamanhoBytePilha(TNO *topo){
  int tamPonteiroMarcador=0;
- int tamanhoByteLista=0;
- // O tamanho dos ponteiros marcadores
- // início e fim são iguais, por essa
- // razão multiplica-se por 2.
- tamPonteiroMarcador += ( sizeof(TNO) )*2;
+ int tamanhoBytePilha=0;
+ // O tamanho do ponteiro topo
+ tamPonteiroMarcador +=  sizeof(TNO) ;
  // Obter tamanho de bytes de todos os
- // elementos da lista
- while(inicio != NULL){
-    tamanhoByteLista += sizeof(*inicio);
-    inicio = inicio->proximo;
+ // elementos da pilha
+ while(topo != NULL){
+    tamanhoBytePilha += sizeof(*topo);
+    topo = topo->proximo;
  }
  printf("\n");
  printf("************************************************\n");
- printf(" Ponteiro marcadores início e fim = %d baytes. *\n",tamPonteiroMarcador);
- printf(" A Fila Encadeada.................= %d baytes. *\n",tamanhoByteLista);
- printf(" Total ...........................= %d baytes. *\n",tamPonteiroMarcador + tamanhoByteLista);
+ printf(" Ponteiro marcador topo = %d baytes. *\n",tamPonteiroMarcador);
+ printf(" A Fila Encadeada.......= %d baytes. *\n",tamanhoBytePilha);
+ printf(" Total .................= %d baytes. *\n",tamPonteiroMarcador + tamanhoBytePilha);
  printf("************************************************\n");
-
+ 
 }
