64f8b3e2cc4a4c52439ff17aa59d6ceb3417af44
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-12-22 11:24:38 -0300

    Prox. Topico: Como retirar estruturas de uma lista

--- Aula/ResumoLinkedList.c
+++ Aula/ResumoLinkedList.c
@@ -18,3 +18,55 @@ Alteramos também a função exibe, que agora exibe a ordem dos elementos na lis
 Conforme podemos ver na imagem no início deste tutorial.
 
 */
+
+/*
+
+--> Como inserir nós em qualquer posição da lista:
+
+Vamos agora mostrar como criar a função insere() que recebe a LISTA e pergunta ao usuário em que posição o usuário quer inserir o elemento na lista. Ou seja, se queremos inserir na posição 'n', o elemento vai ficar nessa posição 'n' e o que estava lá antigamente vai para frente, para posição 'n+1'.
+
+O usuário vai dizer a posição e está sera armazenada na variável pos.
+
+Podemos inserir desde a posição 1 até a 'tam'.
+
+Obviamente, fora desse intervalo devemos mostrar uma mensagem de erro.
+
+Feita essa verificação da posição, vamos adicionar o elemento na dita posição.
+
+Caso seja posição 1, não devemos nos estressar.
+
+Afinal, inserir um elemento na posição 1 é inserir uma estrutura no início da lista, e já criamos uma função para isto, a insereInicio(), bastando chamar ela: insereInicio(LISTA);
+
+Caso seja em qualquer outra posição, a coisa fica mais trabalhosa.
+
+O segredo para isto é identificar dois elementos.o anterior e o elemento que está naquela posição.
+
+Por exemplo, se queremos colocar um nó na terceira posição, devemos guardar essa posição e a anterior, pois iremos fazer o segundo elemento apontar para o novo nó, e fazer esse novo nó apontar para aquele que estava na terceira posição.
+
+Para isso vamos usar dois ponteiros do tipo node, o tipo de nossa estrutura: o 'atual' e o 'anterior'.
+
+O atual começa no primeiro nó da LISTA, e o anterior não está em uma posição anterior (um aponta para LISTA->prox e o outro para LISTA).
+
+Agora temos que fazer estes dois pontos 'correrem' pela lista até chegar onde queremos.
+
+Vamos usar um laço for para isso, e em cada iteração fazemos o seguinte:
+
+Fazemos o ponteiro 'anterior' receber o ponteiro 'atual', e depois fazemos o 'atual' receber o próximo elemento da lista, que é o 'atual -> prox'.
+
+E agora vamos inserir nosso nó, que criamos ao declarar o ponteiro 'novo' e fazer ele receber um bloco alocado pela função aloca().
+
+Vamos lá, devagar pois não é tão simples.
+
+Temos dois nós: o 'atual' e o 'anterior'. Queremos colocar um novo nó, o 'novo', no lugar do nó 'atual' e empurrar o 'atual' pra frente.
+
+Para isso, devemos pegar o 'anterior' e fazer apontar para o 'novo': anterior -> prox = novo;
+
+E o novo nó deve apontar para o que estava nesse posição: novo -> prox = atual;
+
+E claro, incrementar o tamanho da lista: tam++;
+
+Pronto. Já podemos colocar um nó no início, no fim ou em qualquer lugar de nossa lista :)
+
+Vamos para o próximo passo: retirar elementos de nossa lista.
+
+*/
